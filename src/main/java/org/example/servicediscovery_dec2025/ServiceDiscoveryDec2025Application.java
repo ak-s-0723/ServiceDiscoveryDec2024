b4029d24b7f0e8a64716e43f26b09e679ec65280
khannaanurag1 <khannaanurag1@gmail.com>
2025-02-05 20:53:21 +0530

    Initial Commit

---
+++ src/main/java/org/example/servicediscovery_dec2025/ServiceDiscoveryDec2025Application.java
@@ -0,0 +1,13 @@
+package org.example.servicediscovery_dec2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryDec2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryDec2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/servicediscovery_dec2025/ServiceDiscoveryDec2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.servicediscovery_dec2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryDec2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
