55e20cc6eb89fc8d66392160651042198c12a23c
khannaanurag1 <khannaanurag1@gmail.com>
2025-02-05 23:39:42 +0530

    spring cloud

--- src/main/java/org/example/servicediscovery_dec2025/ServiceDiscoveryDec2025Application.java
+++ src/main/java/org/example/servicediscovery_dec2025/ServiceDiscoveryDec2025Application.java
@@ -2,8 +2,10 @@ package org.example.servicediscovery_dec2025;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryDec2025Application {
 
     public static void main(String[] args) {
